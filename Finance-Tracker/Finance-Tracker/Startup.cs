1483ca3e7b2c60913a79201165b50a10398bd10f
alan spencer <alan.spencer24@gmail.com>
2016-04-08 23:14:27 -0400

    Initial

---
+++ Finance-Tracker/Finance-Tracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Finance_Tracker.Startup))]
+namespace Finance_Tracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
